843bc7c78b8bf6151c3ece049815e7afcda7052d
St <stefan.behrendt@t-online.de>
2015-03-31 16:42:15 +0200

    third commit

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ManYearsFromTextField: UITextField!
+    
+    @IBOutlet weak var DogYearsOutput: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ConvertToDogYearsButtonPressed(sender: AnyObject) {
+        
+        let integerAgeFromTextField = ManYearsFromTextField.text.toInt()!
+        let AgeOffset = 7
+        DogYearsOutput.hidden = false
+        DogYearsOutput.text = "\(integerAgeFromTextField * AgeOffset)" + " Dog Years"
+        
+        
+    
+    }
+
+    @IBAction func convertToRealDogYearsButtonPressed(sender: AnyObject) {
+        
+        let stringFromTextField = ManYearsFromTextField.text
+        let doubleFromTextField = Double((stringFromTextField as NSString).doubleValue)
+        var realDogYears:Double
+        
+        if doubleFromTextField > 2 {
+            realDogYears = (10.5 * 2) + (doubleFromTextField - 2) * 4
+        }
+        
+        else {
+            realDogYears = doubleFromTextField * 10.5
+            
+        }
+ mac mini        DogYearsOutput.text = "\(realDogYears)" + " Real Human Years"
 
 }
-
+}
